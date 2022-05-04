1b1eb299f2c946bcc7e5a0bb97114f9c5a249631
Camilo Andres Ferrer Bustos <caferrerb@gmail.com>
2022-05-03 23:12:16 -0500

    test

---
+++ src/test/resources/queries/test_get_devices_by_branch.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.tb_devices (name_device,branch_device,measure_unit,created_at,updated_at) VALUES
+	 ('RTM-234','marca1','Cº',NULL,NULL),
+	 ('LTS-3345','marca1','cm',NULL,NULL),
+	 ('LTS-334','marca1','CEN',NULL,NULL),
+	 ('LTS-331','marca2','CEN',NULL,NULL);
